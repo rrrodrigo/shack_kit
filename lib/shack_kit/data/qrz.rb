6ba0098662111112fdfe1d9fba16d70d86e23157
Marcin Bajer <bajer@tigana.pl>
2016-08-26 11:18:53 +0200

    DRY the QRZ lookup implementation

--- lib/shack_kit/data/qrz.rb
+++ lib/shack_kit/data/qrz.rb
@@ -2,13 +2,13 @@ module ShackKit
   module Data
     class QRZ
       attr_reader :session_key
-      QUERY_URL = 'https://xmldata.qrz.com/xml/'
+      QUERY_URL = 'https://xmldata.qrz.com/xml/current/'
 
       def initialize(params = {})
-        return false unless params[:login] && params[:password] || ShackKit::Data::CONFIG && ShackKit::Data::CONFIG.has_key?('qrz_com')
-        login = params[:login] || ShackKit::Data::CONFIG['qrz_com']['login']
-        password = params[:password] || ShackKit::Data::CONFIG['qrz_com']['password']
-        response = HTTP.post("#{QUERY_URL}/current/", form: { username: login, password: password, agent: "Ruby-gem-shack_kit-#{ShackKit::VERSION}" })
+        return false unless params[:login] && params[:password] || CONFIG && CONFIG.has_key?('qrz_com')
+        login = params[:login] || CONFIG['qrz_com']['login']
+        password = params[:password] || CONFIG['qrz_com']['password']
+        response = HTTP.post(QUERY_URL, form: { username: login, password: password, agent: USER_AGENT })
         parsed_response = Oga.parse_xml(response.to_s)
         @session_key = parsed_response.xpath("QRZDatabase/Session/Key").text
         puts parsed_response.xpath("QRZDatabase/Session/Error").text if @session_key.empty?
@@ -16,7 +16,7 @@ module ShackKit
 
       def lookup(callsign)
         return { error: "Can't query qrz.com, use valid login credentials to get access" } if @session_key.nil? || @session_key.empty?
-        response = HTTP.post("#{QUERY_URL}/current/", form: { s: @session_key, callsign: callsign })
+        response = HTTP.post(QUERY_URL, form: { s: @session_key, callsign: callsign })
         parsed_response = Oga.parse_xml(response.to_s)
         return { error: parsed_response.xpath("QRZDatabase/Session/Error").text } if
           parsed_response.xpath("QRZDatabase/Callsign").text.empty?
